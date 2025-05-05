6bce27208bd2c527916da0309157b3077057e1ab
kaisnavely <158210081+kaisnavely@users.noreply.github.com>
2025-05-05 11:14:18 -0400

    working on testing
    Having issues with it... JUnit wasn't working

---
+++ Java-Minesweeper-Game-master/Java-Minesweeper-Game-master/src/com/zetcode/CoinTest.java
@@ -0,0 +1,14 @@
+package com.zetcode;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CoinTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
